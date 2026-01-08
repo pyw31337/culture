3c42cb7186b9815ead1c5c753d135d0e03b552be
pyw31337 <pyw31337@users.noreply.github.com>
2026-01-08 10:50:20 +0900

    UI: Update OTT category icon

--- src/components/GenreIcons.tsx
+++ src/components/GenreIcons.tsx
@@ -181,9 +181,10 @@ export const HockeyIcon = ({ size = 16 }: { size?: number }) => (
 );
 
 export const OttIcon = ({ size = 16 }: { size?: number }) => (
-    <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-        <rect x="2" y="7" width="20" height="15" rx="2" ry="2"></rect>
-        <polyline points="17 2 12 7 7 2"></polyline>
+    <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-tv-minimal-play-icon lucide-tv-minimal-play">
+        <path d="M15.033 9.44a.647.647 0 0 1 0 1.12l-4.065 2.352a.645.645 0 0 1-.968-.56V7.648a.645.645 0 0 1 .967-.56z" />
+        <path d="M7 21h10" />
+        <rect width="20" height="14" x="2" y="3" rx="2" />
     </svg>
 );
 
