d66bda31509c419346ac53eb91aa2b2b2a242dd2
pyw31337 <pyw31337@users.noreply.github.com>
2026-02-27 17:32:52 +0900

    style: implement rainbow background flow with 25 layers and slide animation

---
+++ src/components/ui/RainbowBackground.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import React from 'react';
+
+export default function RainbowBackground() {
+    return (
+        <div className="fixed inset-0 z-0 overflow-hidden pointer-events-none bg-white light:bg-white dark:bg-white">
+            {/* Rainbow Layers */}
+            {Array.from({ length: 25 }).map((_, i) => (
+                <div key={i} className="rainbow" />
+            ))}
+            
+            {/* Fog/Atmosphere Layers */}
+            <div className="h" />
+            <div className="v" />
+        </div>
+    );
+}
